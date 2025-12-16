b69d27fc94c245079d7d7d6fd6add916334dbe70
alejandro.dev <alejandro.dev@gmail.com>
2025-12-16 12:48:31 +0100

    fix: :zap: Se han eliminado algunos archivos
    eliminacion de inicioPrivado

--- controller/cInicioPublico.php
+++ controller/cInicioPublico.php
@@ -4,6 +4,12 @@
  * @since 16/12/2025
  */
 
+  if(isset($_REQUEST["iniciarSesion"])){
+      $_SESSION['paginaEnCurso']='login';
+      header('location: indexLoginLogoff.php');
+      exit;
+  }
+
   // cargamos el layout principal, y cargará cada página a parte de la estructura principal de la web
   require_once $view['layout'];
  
