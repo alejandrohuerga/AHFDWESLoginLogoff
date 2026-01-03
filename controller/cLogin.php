3c9d44206cfde4b7eeb64c0a4f979cf4df505948
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-01-03 21:30:01 +0100

    feat: :fire: LOGIN FUNCIONAL
    Se ha completado la funcionalidad del login , falta la hora de la ultima conexión anterior

--- controller/cLogin.php
+++ controller/cLogin.php
@@ -1,4 +1,21 @@
-<?php 
+<?php
+    require_once 'core/231018libreriaValidacion.php'; // Importamos la libreria de validación.
+    require_once 'model/UsuarioPDO.php'; // Importamos la clase UsuarioPDO.
+    
+    // Arrays para errores y respuestas
+    $aErrores = [
+        'usuario' => null,
+        'password' => null
+    ];
+
+    $aRespuestas = [
+        'usuario' => '',
+        'password' => ''
+    ];
+
+    // Variable para controlar si la entrada es correcta
+    $entradaOK = true;
+
     // Si pulsa el boton volver , volvemos a la pagina anterior.
     if (isset($_REQUEST["cancelar"])) {
         $_SESSION['paginaEnCurso']='inicioPublico';
@@ -8,9 +25,42 @@
 
     // Si pulsa iniciar Sesión, entramos en Inicio privado.
     if (isset($_REQUEST["entrar"])) {
-        $_SESSION['paginaEnCurso']='inicioPrivado';
-        header("location: indexLoginLogoff.php");  
-        exit;
+        // Guardar página anterior
+        $_SESSION['paginaAnterior'] = $_SESSION['paginaEnCurso'];
+        
+        // Validar los campos del formulario
+        $aErrores['usuario'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['usuario'], 255, 0, 0);
+        $aErrores['password'] = validacionFormularios::validarPassword($_REQUEST['password'], 20, 2, 1, 1);
+        
+        // Guardar las respuestas para rellenar el formulario si hay algun error
+        $aRespuestas['usuario'] = $_REQUEST['usuario'];
+        $aRespuestas['password'] = $_REQUEST['password'];
+        
+        // Verificar si hay errores de validación
+        foreach ($aErrores as $valorCampo=>$msjError) {
+            if ($msjError !=null) {
+                $entradaOK = false;
+            }
+        }
+        
+        // Si la validación es correcta, validar con la BD
+        if ($entradaOK) {
+            $oUsuario = UsuarioPDO::validarUsuario($_REQUEST['usuario'], $_REQUEST['password']);
+
+            
+            if ($oUsuario === null) {
+                $entradaOK = false;
+            } else {
+                // Login correcto
+                $_SESSION['usuarioVGDAWAppLoginLogoff'] = $oUsuario;
+                $_SESSION['paginaEnCurso'] = 'inicioPrivado';
+                header('Location: indexLoginLogoff.php');
+                exit;
+            }
+        }
+    }else {
+        // Si no se ha enviado el formulario
+        $entradaOK = false;
     }
 
     // cargamos el layout principal, y cargará cada página a parte de la estructura principal de la web
--- indexLoginLogoff.php
+++ indexLoginLogoff.php
@@ -1,6 +1,5 @@
 <?php 
-   // Iniciamos la sesión
-   session_start();
+   
 /**
  * @author Alejandro De la Huerga
  * @since 16/12/2025
@@ -9,7 +8,10 @@
    // Cargamos los archivos de configuración
    require_once 'config/confAPP.php';
    require_once 'config/confDBPDO.php';
+   require_once 'model/Usuario.php';
    
+   // Iniciamos la sesión
+   session_start();
 
    // Si no esta la página en curso con la sesión la creamos con inicio público.
    if(!isset($_SESSION['paginaEnCurso'])){
