c62a625fa927624118a6448ccf6c5552efacbf88
alejandro.dev <alejandro.dev@gmail.com>
2026-01-09 12:35:46 +0100

    fix: Cambio imagen Inicio público
    Se ha cambiado la estructura del html y la imagen de inicio público

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -16,18 +16,5 @@
     </form>
 </header>
 <main>
-    <div class="container">
-        <div class="carousel">
-            <input type="radio" name="rd" id="rd1">
-            <input type="radio" name="rd" id="rd2">
-            <input type="radio" name="rd" id="rd3">
-            <input type="radio" name="rd" id="rd4">
-            <div class="photos">
-                <img src="doc/images/caminoProgramado.PNG" alt="Camino Programado">
-                <img src="doc/images/diagramaClases.png" alt="Diagrama de Clases">
-                <img src="doc/images/modeloFisicoDatos.png" alt="Modelo Fisico de Datos">
-                <img src="doc/images/relacionFicheros.png" alt="Relacion de ficheros">
-            </div>
-        </div>
-    </div>
+    <img src="doc/images/caminoProgramado.PNG" alt="Camino Programado">
 </main>
