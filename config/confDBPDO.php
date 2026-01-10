e428b95fd24db07d2a8beec56edab805c5eed10a
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-01-10 17:15:05 +0100

    fix: :bug: Se hamodificado el fichero de configuracion de la base de datos
    Se ha agregado la conf de la base de datos para explotacion

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -5,10 +5,15 @@
     define('PSWD', 'paso');
     */
 
-    /* CONF Entorno de desarrollo Casa */
+    /* CONF Entorno de desarrollo Casa 
     define('DNS', 'mysql:host=192.168.1.100;dbname=DBAHFDWESLoginLogoff;charset=utf8');
     define('USUARIODB', 'userAHFDWESLoginLogoff');
     define('PSWD', 'paso');
-    
+    */
+
+    /* CONF Entorno de Explotación */
+    define('DNS', 'mysql:host=localhost;dbname=DBAHFDWESLoginLogoff;charset=utf8');
+    define('USUARIODB', 'userAHFDWESLoginLogoff');
+    define('PSWD', 'Huerga2002');
     
 ?>
