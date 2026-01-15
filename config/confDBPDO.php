95ffa3d3bd9b7a42324de5888c2c9a9d7ed54157
alejandro.dev <alejandro.dev@gmail.com>
2026-01-15 13:31:40 +0100

    feat: :bug: Cambio en el fichero de configuración
    Se ha cambiado el fichero para explotación

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -1,9 +1,9 @@
 <?php
-    /* CONF Entorno de Desarrollo Clase */
+    /* CONF Entorno de Desarrollo Clase 
     define('DNS', 'mysql:host=local.ieslossauces.es;dbname=DBAHFDWESLoginLogoff;charset=utf8');
     define('USUARIODB', 'userAHFDWESLoginLogoff');
     define('PSWD', 'paso');
-    
+    */
 
     /* CONF Entorno de desarrollo Casa 
     define('DNS', 'mysql:host=192.168.1.100;dbname=DBAHFDWESLoginLogoff;charset=utf8');
@@ -11,10 +11,10 @@
     define('PSWD', 'paso');
     */
 
-    /* CONF Entorno de Explotación 
+    /* CONF Entorno de Explotación */
     define('DNS', 'mysql:host=localhost;dbname=DBAHFDWESLoginLogoff;charset=utf8');
     define('USUARIODB', 'userAHFDWESLoginLogoff');
     define('PSWD', 'Huerga2002');
-    */
+    
     
 ?>
